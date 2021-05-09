495c7ee29d77e4d312ecc15fed58023a0a1c3d88
rdnply <polyanscky.rodion@yandex.ru>
2021-05-09 21:54:25 +0300

    add turn on/off local camera

--- web/panel.js
+++ web/panel.js
@@ -1,5 +1,4 @@
 var isMuted = true;
-
 let processAudioBtn = document.getElementById('processAudio');
 
 function processAudio() {
@@ -14,3 +13,25 @@ function processAudio() {
 
 processAudioBtn.onclick = processAudio;
 
+
+var isEnableVideo = true;
+let processVideoBtn = document.getElementById('processVideo')
+
+function processVideo() {
+    if (isEnableVideo) {
+        let video = document.getElementById('localVideo');
+        let stream = video.srcObject;
+
+        stream.getVideoTracks().forEach(track => track.stop());
+        video.srcObject = new MediaStream(stream.getAudioTracks());
+    } else {
+        navigator.mediaDevices.getUserMedia({video: true, audio: true})
+            .then(stream => {
+                document.getElementById('localVideo').srcObject = stream
+                // stream.getTracks().forEach(track => pc.addTrack(track, stream))
+            })
+    }
+    isEnableVideo = !isEnableVideo;
+}
+
+processVideoBtn.onclick = processVideo
